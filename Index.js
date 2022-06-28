b2ae5fabe5ba9f3c0bc40e05721558412c315b1d
Suman Bera <suman80bera@gmail.com>
2022-06-28 23:24:58 +0530

    update

--- Index.js
+++ Index.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://gautamku1122:Sae755%40gautam@gautam.p4ovs.mongodb.net/Project02-Intern", {
+mongoose.connect("mongodb+srv://Suman-1432:Suman1432@cluster0.bkkfmpr.mongodb.net/group74Database", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))

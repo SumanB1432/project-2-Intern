1c3832556d8e16bd160868f291eb5fb93dc619c7
Suman Bera <suman80bera@gmail.com>
2022-06-30 18:23:55 +0530

    update

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,6 +1,13 @@
 const collegeModel = require("../models/collegeModel");
 const internModel = require("../models/internModel");
 
+const isValid = function (value) {
+    if (typeof value === "undefined" || value === null) return false
+    if (typeof value === "string" && value.trim().length === 0) return false
+    if (typeof value === Number && value.trim().length === 0) return false
+    return true
+}
+
 const createCollege = async function (req, res) {
     try {
         let data = req.body;
@@ -9,6 +16,10 @@ const createCollege = async function (req, res) {
             return res.status(400).send({ status: false, message: "You must enter data" })
         }
         if (!data.name) return res.status(400).send({ status: false, message: "You must enter name" })
+
+        if (!isValid(data.name)) {
+            return res.status(400).send({ status: false, msg: "Please Enter college Name" })
+        }
 
         if (!data.name.trim().match(/^[a-zA-Z]+$/)) {
             return res.status(400).send({ status: false, msg: "Enter a valid name." })
@@ -19,11 +30,20 @@ const createCollege = async function (req, res) {
 
         if (!data.fullName) return res.status(400).send({ status: false, message: "You must enter full name" })
 
+        if (!isValid(data.fullName)) {
+            return res.status(400).send({ status: false, msg: "Please Enter college fullName " })
+        }
+
         if (!data.fullName.trim().match(/^[a-zA-Z,\-.\s]*$/)) {
             return res.status(400).send({ status: false, msg: "Enter a valid full name." })
         }
 
         if (!data.logoLink) return res.status(400).send({ status: false, message: "You must enter logoLink" })
+
+        if (!isValid(data.logoLink)) {
+            return res.status(400).send({ status: false, msg: "Please Enter logo Link" })
+        }
+
         if (!data.logoLink.trim().match(/^(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%.\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%\+.~#?&//=]*)$/))
             return res.status(400).send({ status: false, msg: "Enter a valid logo link" })
 
--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,6 +1,13 @@
 const internModel = require("../models/internModel")
 const collegeModel = require("../models/collegeModel")
 
+const isValid = function (value) {
+    if (typeof value === "undefined" || value === null) return false
+    if (typeof value === "string" && value.trim().length === 0) return false
+    if (typeof value === Number && value.trim().length === 0) return false
+    return true
+}
+
 const createInterns = async function (req, res) {
     try {
         let data = req.body;
@@ -9,16 +16,29 @@ const createInterns = async function (req, res) {
             return res.status(400).send({ status: false, message: "You must enter data" })
         }
         if (!data.name) return res.status(400).send({ status: false, message: "You must enter name" })
+
+        if (!isValid(data.name)) {
+            return res.status(400).send({ status: false, msg: "Please Enter intern Name" })
+        }
+
         if (!data.name.trim().match(/^[a-zA-Z,\s]*$/)) {
             return res.status(400).send({ status: false, msg: "Enter a valid name." })
         }
         if (!data.email) return res.status(400).send({ status: false, message: "You must give emailId" })
+
+        if (!isValid(data.email)) {
+            return res.status(400).send({ status: false, msg: "Please Enter email" })
+        }
         if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email.trim()))) {
             return res.status(400).send({ status: false, msg: "Enter a valid email address." })
         }
         let isRegisteredEmail = await internModel.find({ email: data.email });
         if (isRegisteredEmail.length) return res.status(400).send({ status: false, message: "email id already registered" });
         if (!data.mobile) return res.status(400).send({ status: false, message: "You must give mobile number" })
+
+        if (!isValid(data.mobile)) {
+            return res.status(400).send({ status: false, msg: "Please Enter Mobile Number" })
+        }
         if (!data.mobile.trim().match(/^[6-9]\d{9}$/)) {
             return res.status(400).send({ status: false, msg: "Enter a valid mobile number" })
         }
@@ -26,6 +46,10 @@ const createInterns = async function (req, res) {
         if (isRegisteredMobile.length) return res.status(400).send({ status: false, msg: "mobile number already registered" });
 
         if (!data.collegeName) return res.status(400).send({ status: false, message: "You must give college name" })
+
+        if (!isValid(data.collegeName)) {
+            return res.status(400).send({ status: false, msg: "Please Enter college Name" })
+        }
         if (!data.collegeName.trim().match(/^[a-zA-Z]+$/)) {
             return res.status(400).send({ status: false, msg: "Enter a valid college name." })
         }
